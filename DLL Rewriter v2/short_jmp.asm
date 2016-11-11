a5b695518011761d65a2bf544138b1b1e06dda32
Redford <mkow13@gmail.com>
2016-11-11 01:10:52 +0100

    Added DLL Rewriter v2, currently just a copy of v1

---
+++ DLL Rewriter v2/short_jmp.asm
@@ -0,0 +1,20 @@
+; Położenie tego kodu zostanie ustawione na RVA pamięci, w której
+; docelowo ma się on znaleźć (za pomocą dyrektywy ORG asemblera nasm).
+__begin_marker: ; Etykieta używana przez nasz parser plików .map
+
+%macro redirect 2 ; Argumenty: adres funkcji (RVA), indeks
+  longjmp_%2:
+    jmp %1 ; Skok do oryginalnej funkcji. Ponieważ jmp jest skokiem
+           ; względnym, nie musimy przejmować się, gdzie biblioteka
+           ; zostanie załadowana do pamięci (czyli znać docelowego VA)
+  times 5 nop    ; umożliwia hot-patching
+  align 16, nop  ; wyrównanie adresu wpisu do 16 bajtów
+  entry_%2: ; Etykieta entry_<indeks> zostanie użyta jako
+            ; nowy adres funkcji o tym indeksie
+    jmp short longjmp_%2 ; Pierwsza instrukcja musi zajmować
+                         ; przynajmniej 2 bajty (hot-patching
+                         ; tego wymaga)
+  ; Alternatywna wersja:
+  ;je %1
+  ;jne %1
+%endmacro
--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Repozytorium to zawiera pełny kod narzędzia przedstawionego w rozdziale 9. ksi
 
 Jest to wersja kodu kompatybilna z listingami zawartymi w książce, łącznie z formatowaniem kodu. Będę przyjmował pull-requesty, o ile będą dotyczyć istotnych bugów.
 
-## DLL Rewriter v2
+## DLL Rewriter v2 [Wersja rozwojowa]
 
 Wersja ta zawiera wiele usprawnień w stosunku do v1, przez co nie jest kompatybilna z listingami z książki. Możliwe że będzie się nieco rozwijać z biegiem czasu. Wszelkie pull-requesty i zgłoszenia bugów mile widziane.
